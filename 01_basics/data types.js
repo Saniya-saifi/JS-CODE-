d554845666f8ae2e6c481a420170510ae606d818
Saniya-saifi <saniyasaifi4401@gmail.com>
2025-01-03 13:03:11 +0000

    data types

--- 01_basics/data types.js
+++ 01_basics/data types.js
@@ -1,1 +1,26 @@
-"use strict" //
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node js  not browser
+
+console.log(3+3)
+console.log("saniya saifi")
+
+let name= saniya
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null =>stanalone value
+// undefined =>
+// symbol => unique
+
+
+
+//object 
+
+console.log( typeof undefined); // undefined
+console.log(typeof null); // object
+
