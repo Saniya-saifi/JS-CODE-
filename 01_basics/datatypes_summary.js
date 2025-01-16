5843dc3ca49028001e6268a39ceaa7df9505d813
Saniya-saifi <saniyasaifi4401@gmail.com>
2025-01-16 14:22:25 +0000

    done

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,11 @@
+//  PRIMITIVES DATA TYPES
+
+// 7 types : String ,Number, Boolean, null, Undefinied, Symbol, BigInt
+
+// 
+
+// REFERENCE/ NON PRIMITIVE DATA TYPES
+
+// Arrays, Objects, Functions
+
+
